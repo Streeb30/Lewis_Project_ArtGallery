71455966ef3f358ed6d651f8f4a1e4bbf2eb51c4
LewisHe <streeb30@gmail.com>
2024-06-28 16:15:02 -0400

    modified server.js

--- server.js
+++ server.js
@@ -9,21 +9,24 @@ const userRoutes = require('./user-routes'); //require user routes
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); // Handle URL-encoded data
 app.use(express.json()); // Body parser middleware
 
 app.use(session({
-    secret: 'Lewis',
+    secret: process.env.SESSION_SECRET || 'default_secret',
     resave: false,
     saveUninitialized: false
 }));
 
 // MongoDB connection setup
-mongoose.connect('mongodb://localhost:27017/user_database', {});
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -36,17 +39,18 @@ db.once('open', async () => {
     } catch (error) {
         console.error('Error loading data and artist accounts:', error);
     }
-    // Start the server
-    app.listen(PORT, () => {
-        console.log(`Server listening at http://localhost:${PORT}`);
-    });
 });
 
 app.get('/', (req, res) => {
     res.render('home-page');
 });
 
-// Routes setup
-app.use('/artist', artistRoutes);
-app.use('/patron', patronRoutes);
-app.use('/user', userRoutes);
+// Routes test setup
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server listening at http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;

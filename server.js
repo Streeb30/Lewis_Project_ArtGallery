d1bcf50b2db58404c37a7a61c188b88d15a2124d
LewisHe <streeb30@gmail.com>
2024-06-30 11:02:07 -0400

    com

--- server.js
+++ server.js
@@ -28,13 +28,8 @@ app.use('/patron', patronRoutes);
 app.use('/artist', artistRoutes); 
 
 // MongoDB connection setup
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
-    sslValidate: true
-  });
-  
+mongoose.connect(process.env.MONGODB_URI);
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', async () => {
